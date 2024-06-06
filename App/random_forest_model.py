ac7c0fa4e073969ac53fbf9367edb5ab51f2216e
thaovann <130448361+thaovann@users.noreply.github.com>
2024-06-06 22:54:17 +0700

    Update random_forest_model.py

--- App/random_forest_model.py
+++ App/random_forest_model.py
@@ -1,11 +1,8 @@
 from sklearn.ensemble import RandomForestClassifier
 from data_preprocessing import X_train, X_test, y_train, y_test
 
-# Create and train the Random Forest model
+
 rf = RandomForestClassifier(n_estimators=100, max_features="sqrt")
 rf.fit(X_train, y_train)
 
-
-
-# Make predictions
 y_pred_rf = rf.predict(X_test)
