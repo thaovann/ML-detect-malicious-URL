fc72567ae6ec517fbbb523d0ce8655dc35ecc5cf
thaovann <130448361+thaovann@users.noreply.github.com>
2024-06-06 22:55:01 +0700

    Update add_new_data.py

--- addNewData/add_new_data.py
+++ addNewData/add_new_data.py
@@ -1,9 +1,6 @@
 import pandas as pd
-
-# Load the existing CSV file
 existing_data = pd.read_csv("./MaliciousURL.csv")
 
-# Collect user input for new data
 new_urls = []
 new_types = []
 
@@ -16,11 +13,7 @@ for i in range(num_entries):
     url_type = input(f"Enter type for URL {url}: ")
     new_types.append(url_type)
 
-# Create a DataFrame with the new data
 new_data = pd.DataFrame({"url": new_urls, "type": new_types})
 
-# Append the new data to the existing DataFrame
 updated_data = pd.concat([existing_data, new_data], ignore_index=True)
-
-# Write the updated DataFrame back to the CSV file
 updated_data.to_csv("./malicious_phish.csv", index=False)
